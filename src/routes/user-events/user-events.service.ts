f6aa20d1c969a4874d4b0830b2486e8b902ed9a2
Jenn Duong <itsjennyduong@gmail.com>
2024-11-24 17:23:27 -0800

    Add PATCH user-events endpoint - Create schema - Create service

--- src/routes/user-events/user-events.service.ts
+++ src/routes/user-events/user-events.service.ts
@@ -6,3 +6,31 @@ const db = knex(knexConfig.development);
 export async function getAllUserEvents() {
   return db("user_events").select("*");
 }
+
+export async function updateUserEventById(
+  updatedAttending: null | boolean,
+  userEventId: number
+) {
+  try {
+    const updatedUserEvent = await knex("user_events")
+      .where({ id: userEventId })
+      .update(
+        {
+          attending: updatedAttending,
+          updated_at: db.fn.now(),
+        },
+        ["id", "attending"]
+      )
+      .returning("*");
+
+    if (updatedUserEvent.length ===0) {
+      //if no user_event was updated 
+      return null;
+    }
+
+    return updatedUserEvent[0];
+  }catch(error){ 
+    console.error("Error updating user_events:", error);
+    throw new Error("Failed to update user_events");
+  }
+}
--- src/routes/user-events/user-events.ts
+++ src/routes/user-events/user-events.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { getAllUserEvents } from "./user-events.service";
-import { getUserEventsSchema } from "./user-events.schema";
+import { getAllUserEvents, updateUserEventById } from "./user-events.service";
+import { getUserEventsSchema, updateUserEventSchema } from "./user-events.schema";
 
 export async function userEventsRoutes(fastify: FastifyInstance) {
   fastify.get(
@@ -13,6 +13,44 @@ export async function userEventsRoutes(fastify: FastifyInstance) {
       const eventCount = events.length;
 
       reply.send({ events, count: eventCount });
+    }
+  );
+
+
+  fastify.patch(
+    "/user-events/:id",
+    {
+        schema: updateUserEventSchema as any, 
+    },
+    async (request, reply) => { 
+      const { id } = request.params as { id: number };
+      const { attending } = request.body as {
+        attending: null | boolean;
+      };
+
+      try {
+        const updatedUserEvent = await updateUserEventById(
+          attending,
+          id 
+        );
+
+        if (!updatedUserEvent) {
+          return reply.status(404).send({ message: "User_events update failed"});
+        }
+
+        reply
+          .status(200)
+          .send({ message: "User_events updates successfully", user_event: updatedUserEvent});
+
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          reply 
+            .status(500)
+            .send({ message: "error updating user_event", error: error.message});
+        } else {
+          reply.status(500).send({ message: "Something broke bro"});
+        }
+      }
     }
   );
 }
